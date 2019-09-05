00e810014a61277a5c6a0b09589845c747634b3a
vasamvishal <54842886+vasamvishal@users.noreply.github.com>
2019-09-05 19:54:25 +0530

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+	 class Math{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+       
+      double n=24;
+      if(n>=0&&n<31)
+      {
+      for(int i=0;i<n;i++)
+      {
+    	  double x=math.pow(2,i);
+    	  System.out.println("2*"+i+"="+x);
+      }
+	}
+
+}
+
+     
+}
