aed49cda7652bd4fdc94c4b5054eb8978f5f937d
vasamvishal12@gmail.com <shalurajput27@gmail.com>
2019-09-23 19:27:07 +0530

    23/09/2019

--- algorithmproblem/mochatest1/paranthesisTest.js
+++ algorithmproblem/mochatest1/paranthesisTest.js
@@ -5,49 +5,49 @@ var q=new stack.Stack();
 describe('paranchecker problem', function()  
 { 
     it('push function should return null for undefined.', function()  
-{  
+    {  
     assert.equal(q.push(),null);   
-});
+   });
 
     it('push function should return undefined for  character.', function()  
-{  
+    {  
     assert.equal(q.push('m'),undefined);   
-});
+    });
 it('print should return value .', function()  
-{  
+    {  
   assert.equal(q.printStack(),"m ");   
-}); 
+     }); 
 it('pop should return the value .', function()  
-{  
+    {  
   assert.equal(q.pop(),'m');   
-});
+    });
 
 it('push function should return undefined for  character.', function()  
-{  
+    {  
     assert.equal(q.push('m'),undefined);   
-});
+    });
 it('print should return value .', function()  
-{  
-  assert.equal(q.printStack(),"m ");   
-});
+    {  
+    assert.equal(q.printStack(),"m ");   
+    });
  it('addrear function should return null for negative values.', function()  
-{  
+      {  
      assert.equal(q.push(-10),null);   
- });
+      });
  
    it('pop should return the value .', function()  
-   {  
+      {   
      assert.equal(q.pop(),'m');   
-   });
+     });
    it('pop should return undefined for no returning  value .', function()  
-   {  
+       {  
      assert.equal(q.pop(),undefined);   
-   });
+        });
 
    it('print should return  value .', function()  
-  {  
+   {  
   assert.equal(q.printStack(),'');   
-  });
+    });
 
   
 
